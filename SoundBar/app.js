82f394d453c25ccb9b04ae801c0e1bf90a04a27e
Luiz Gabriel <91032465+oluizbs@users.noreply.github.com>
2023-01-19 14:44:10 -0300

    clock

---
+++ SoundBar/app.js
@@ -0,0 +1,6 @@
+const ratinho = document.querySelector('#button').addEventListener('click', ()=>{
+    const audio = document.querySelector('#audio')
+    audio.correntTime = 0
+    audio.play()
+    console.log("teste")
+})
---
+++ lógica/LacosRepeticao/app.js
@@ -0,0 +1,17 @@
+// Forma "errada"
+// console.log("Olá");
+// console.log("Olá");
+// console.log("Olá");
+// console.log("Olá");
+// console.log("Olá");
+
+// Forma correta com For
+// for(let i = 0; i < 5; i++ ){
+//     console.log('Olá', i);
+// }
+
+// Forma correta com While
+// let i = 0;
+// while(i < 5){
+//     console.log('Olá', i++);
+// }
